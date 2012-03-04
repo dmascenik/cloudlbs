d5e0ba174b4dcac63e83ef0a2062b43120fac66a
Dan Mascenik <dan@cloud-lbs.com>
2012-03-04 14:39:57 -0500

    Switched web.xml to JavaConfig-based loading

--- webapp/src/main/java/com/cloudlbs/web/server/WebAppSpringConfig.java
+++ webapp/src/main/java/com/cloudlbs/web/server/WebAppSpringConfig.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletContext;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.context.ServletContextAware;
 
@@ -12,6 +13,7 @@ import com.cloudlbs.web.service.LoginService;
 import com.cloudlbs.web.service.LoginServiceImpl;
 
 @Configuration
+@ImportResource("classpath:/spring/spring-http-security.xml")
 public class WebAppSpringConfig implements ServletContextAware {
 
     @Inject private AuthenticationManager authenticationManager;
